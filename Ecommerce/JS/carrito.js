5852687b241de132405b41d5689a1bd1bc91c697
Rosalía <84238521+Ro07-r@users.noreply.github.com>
2023-09-14 19:44:57 -0300

    Subo parte del carrito.js

---
+++ Ecommerce/JS/carrito.js
@@ -0,0 +1,25 @@
+const productosEncarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+
+if (productosEnCarrito) {
+
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+
+    productosEncarrito.forEach(producto => {
+        
+        const div = document.createElement("div");
+        div.classList.add("carrito-producto");
+    });
+
+    
+
+} else {
+
+}

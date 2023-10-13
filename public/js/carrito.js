d574dd55f08a6843dacf1eb786e0f0a1a94dbcf2
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-13 19:25:06 -0300

    Update carrito.js

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -25,24 +25,26 @@ function cargarProductosCarrito() {
             const div = document.createElement("div");
             div.classList.add("carrito-producto");
             div.innerHTML = `
-                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-                <div class="carrito-producto-titulo">
+                <img class="item-carrito carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="item-carrito carrito-producto-titulo">
                     <small>Producto</small>
                     <h3>${producto.titulo}</h3>
                 </div>
-                <div class="carrito-producto-cantidad">
-                    <small>Cantidad</small>
-                    <p>${producto.cantidad}</p>
+                <div class="item-carrito-row">
+                    <div class="item-carrito carrito-producto-cantidad">
+                        <small>Cantidad</small>
+                        <p>${producto.cantidad}</p>
+                    </div>
+                    <div class="item-carrito carrito-producto-precio">
+                        <small>Precio</small>
+                        <p>$${producto.precio}</p>
+                    </div>
+                    <div class="item-carrito carrito-producto-subtotal">
+                        <small>Subtotal</small>
+                        <p>$${producto.precio * producto.cantidad}</p>
+                    </div>
+                    <button class="item-carrito carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
                 </div>
-                <div class="carrito-producto-precio">
-                    <small>Precio</small>
-                    <p>$${producto.precio}</p>
-                </div>
-                <div class="carrito-producto-subtotal">
-                    <small>Subtotal</small>
-                    <p>$${producto.precio * producto.cantidad}</p>
-                </div>
-                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
             `;
     
             contenedorCarritoProductos.append(div);

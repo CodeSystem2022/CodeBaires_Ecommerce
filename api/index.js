a6e67b1863a8bc08389b0ff5c529e00289b28330
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-29 23:06:42 -0300

    Fix carrito

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const mercadopago = require("mercadopago");
 const { getProducts } = require("./src/services/productService");
 
 mercadopago.configure({
-  access_token: "APP_USR-2627316415575972-102917-6b1926269037590c4e01508a8e8b16de-1513494519",
+  access_token: process.env.MP_ACCESS_TOKEN,
 });
 
 app.use(express.urlencoded({ extended: false }));
